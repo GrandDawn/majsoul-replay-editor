a061fe47e341443f9ce9bc938d8224567cb2ec3e
xiaolan16 <69021379+xiaolan16@users.noreply.github.com>
2021-06-15 19:21:52 +0800

    Create 2.js

---
+++ examples/2.js
@@ -0,0 +1,86 @@
+try{MRE.open();}catch(e){};
+editdata.player_datas[0].nickname="电脑(简单)";
+editdata.player_datas[1].nickname="电脑(简单)";
+editdata.player_datas[2].nickname="电脑(简单)";
+editdata.player_datas[3].nickname="电脑(简单)";
+editdata.player_datas[0].avatar_id=400101;
+editdata.player_datas[1].avatar_id=400101;
+editdata.player_datas[2].avatar_id=400101;
+editdata.player_datas[3].avatar_id=400101;
+settings.chuanma_points_method=0;
+editdata.config={
+  'category':1,
+  'meta':{'mode_id':2},
+  'mode':{
+    'mode':11,
+    'detail_rule':{
+      'begin_open_mode':0,
+      'chuanma':0,
+      'dora3_mode':0,
+      'dora_count':3,
+      'fanfu':1,
+      'guyi_mode':0,
+      'have_zimosun':false,
+      'huansanzhang':0,
+      'open_hand':0,
+      'init_point':35000,
+      'muyu_mode':0,
+      'shiduan':1,
+      'xuezhandaodi':0,
+      'xueliu':0
+    }
+  }
+}
+tiles0=["1s","1s","1s","1z","1z","1z","5z","5z","6z","6z","7z","7z","7z","2z"];
+tiles1=["1m","2s","2s","2s","3s","3s","3s","4s","4s","4s","6s","8s","8s"];
+tiles2=["1m","1p","1p","1p","2p","2p","2p","3p","3p","3p","6s","8s","8s"];
+tiles3=[];
+paishan=randompaishan("6s6s4z","2s4s5z2p3p6z9s3z9s3z3z9s6z9s3z5z1m7z1z1s4z4z4z");
+roundbegin();
+qiepai("2z",true);
+mopai();
+qiepai("1m",true);
+mopai();
+qiepai("1m",true);
+mopai();
+leimingpai();
+mopai();
+leimingpai();
+mopai();
+leimingpai();
+mopai();
+leimingpai();
+mopai();
+leimingpai();
+mopai();
+leimingpai();
+mopai();
+leimingpai();
+mopai();
+qiepai();
+for(let i=1;i<=14;i++){
+  mopai();
+  qiepai();
+  mopai();
+  qiepai();
+  mopai();
+  qiepai();
+}
+mopai();
+leimingpai();
+mopai();
+qiepai();
+hupai();
+roundend();
+
+tiles0="1s1s2s2s3s3s4s5s6s7s8s9s9s4z";
+tiles1=["1m","2s","2s","2s","3s","3s","3s","4s","4s","4s","6s","8s","8s"];
+tiles2=["1m","1p","1p","1p","2p","2p","2p","3p","3p","3p","6s","8s","8s"];
+tiles3=[];
+paishan=randompaishan("","9s");
+roundbegin();
+leimingpai();
+mopai();
+hupai();
+roundend();
+try{MRE.close();}catch(e){};
